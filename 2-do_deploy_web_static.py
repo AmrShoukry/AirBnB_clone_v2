0caf85e873d37f05d0db4655188c63eb4bb6e59a
Amr Shoukry <shoukryworkamr1@gmail.com>
2023-09-07 11:30:09 +0300

    adding @task

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 """ 2. Deploy archive! """
 import os
-from fabric.api import env, run, put
+from fabric.api import env, run, put, task
 
 env.hosts = ['18.207.140.20', '18.204.13.232']
 env.user = 'ubuntu'
 env.key_filename = '~/.ssh/school'
 
-
+@task
 def do_deploy(archive_path):
     """ Deploying a new archive into web servers """
     
@@ -17,16 +17,14 @@ def do_deploy(archive_path):
     try:
         put(archive_path, '/tmp/')
         archive_name = os.path.basename(archive_path)
-        run(f'mkdir -p /data/web_static/releases/{archive_name}/')
-        run(f'tar -xzf /tmp/{archive_name} -C\
-            /data/web_static/releases/{archive_name}')
+        path = "/data/web_static/releases/"
+        run(f'mkdir -p {path}{archive_name}/')
+        run(f'tar -xzf /tmp/{archive_name} -C {path}{archive_name}')
         run(f'rm /tmp/{archive_name}')
-        run(f'mv /data/web_static/releases/{archive_name}/web_static/*\
-             /data/web_static/releases/{archive_name}/')
-        run(f'rm -rf /data/web_static/releases/{archive_name}/web_static')
+        run(f'mv {path}{archive_name}/web_static/* {path}{archive_name}/')
+        run(f'rm -rf /{path}{archive_name}/web_static')
         run(f'rm -rf /data/web_static/current')
-        run(f'ln -s /data/web_static/releases/{archive_name}/\
-             /data/web_static/current')
+        run(f'ln -s {path}{archive_name}/ /data/web_static/current')
         print("New version deployed!")
         return True
     except Exception as E:
