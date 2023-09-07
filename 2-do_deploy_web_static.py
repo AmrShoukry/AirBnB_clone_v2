818b7b9e21bad9424076257b77c7958fb1eed91a
Amr Shoukry <shoukryworkamr1@gmail.com>
2023-09-07 14:10:39 +0300

    without extension

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -14,16 +14,17 @@ def do_deploy(archive_path):
 
     archive_name = os.path.basename(archive_path)
     path = "/data/web_static/releases/"
+    archive_only = archive_name.split('.')[0]
 
     try:
         put(archive_path, '/tmp/')
-        run(f'mkdir -p {path}{archive_name}/')
-        run(f'tar -xzf /tmp/{archive_name} -C {path}{archive_name}')
+        run(f'mkdir -p {path}{archive_only}/')
+        run(f'tar -xzf /tmp/{archive_name} -C {path}{archive_only}')
         run(f'rm /tmp/{archive_name}')
-        run(f'mv {path}{archive_name}/web_static/* {path}{archive_name}/')
-        run(f'rm -rf {path}{archive_name}/web_static')
+        run(f'mv {path}{archive_only}/web_static/* {path}{archive_only}/')
+        run(f'rm -rf {path}{archive_only}/web_static')
         run(f'rm -rf /data/web_static/current')
-        run(f'ln -s {path}{archive_name}/ /data/web_static/current')
+        run(f'ln -s {path}{archive_only}/ /data/web_static/current')
         print("New version deployed!")
         return True
     except Exception as E:
