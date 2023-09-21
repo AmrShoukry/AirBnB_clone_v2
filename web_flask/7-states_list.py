7553d460d75722d97fe91cea8a7010b6b299edf8
Amr Shoukry <shoukryworkamr1@gmail.com>
2023-09-21 20:01:45 +0300

    pycodestyle

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 """ 6-number_odd_or_even.py """
 
+from models import storage
 from flask import Flask, render_template
 app = Flask(__name__)
-
-
-from models import storage
 
 
 @app.teardown_appcontext
@@ -17,7 +15,8 @@ def teardown(exception):
 @app.route("/states_list", strict_slashes=False)
 def odd_even():
     """ 0.0.0.0:5000/hbnb/states_list """
-    return render_template('7-states_list.html', data=storage.all('State').values())
+    return render_template('7-states_list.html',
+                           data=storage.all('State').values())
 
 
 if (__name__ == '__main__'):
