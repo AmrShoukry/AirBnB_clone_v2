8471d7c5f9cfdbc08cfe34d4b229cfe91f3ca937
Amr Shoukry <shoukryworkamr1@gmail.com>
2023-09-21 22:54:24 +0300

    hbnb

--- web_flask/100-hbnb.py
+++ web_flask/100-hbnb.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" 10-hbnb_filters.py """
+""" 100-hbnb.py """
 
 from models import storage
 from flask import Flask, render_template
@@ -12,17 +12,16 @@ def teardown(exception):
     storage.close()
 
 
-@app.route("/hbnb_filters", strict_slashes=False)
-def filters():
-    """ 0.0.0.0:5000/hbnb_filters """
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """ 0.0.0.0:5000/hbnb """
     state_objects = storage.all('State').values()
     states = sorted(state_objects, key=lambda state: state.name)
 
     amenities_objects = storage.all('Amenity').values()
     amenities = sorted(amenities_objects, key=lambda amenity: amenity.name)
 
-    return render_template('10-hbnb_filters.html', states=states,
-                           amenities=amenities)
+    return render_template('100-hbnb.html', states=states, amenities=amenities)
 
 
 if (__name__ == '__main__'):
